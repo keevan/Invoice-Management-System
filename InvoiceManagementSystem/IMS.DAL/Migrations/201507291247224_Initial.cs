7fb976a8319962a5e3c5b0ec4a75626c4af0e4d8
keevan <keevan.pham@gmail.com>
2015-07-29 23:01:14 +1000

    Established connection to AppHabor and rebased migrations for a clean start. (actually added all things)

---
+++ InvoiceManagementSystem/IMS.DAL/Migrations/201507291247224_Initial.cs
@@ -0,0 +1,101 @@
+namespace IMS.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        BasketItemId = c.Int(nullable: false, identity: true),
+                        BasketId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BasketItemId)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .ForeignKey("dbo.Baskets", t => t.BasketId, cascadeDelete: true)
+                .Index(t => t.BasketId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(maxLength: 255),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Description = c.String(),
+                        ImageUrl = c.String(maxLength: 255),
+                        CostPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        BasketId = c.Int(nullable: false, identity: true),
+                        date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.BasketId);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerId = c.Int(nullable: false, identity: true),
+                        CustomerName = c.String(),
+                        Address1 = c.String(),
+                        Town = c.String(),
+                        Postcode = c.String(),
+                    })
+                .PrimaryKey(t => t.CustomerId);
+            
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        OrderItemId = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Order_OrderId = c.Int(),
+                    })
+                .PrimaryKey(t => t.OrderItemId)
+                .ForeignKey("dbo.Orders", t => t.Order_OrderId)
+                .Index(t => t.Order_OrderId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        OrderDate = c.DateTime(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                        OrderLocation = c.String(maxLength: 255),
+                    })
+                .PrimaryKey(t => t.OrderId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderItems", "Order_OrderId", "dbo.Orders");
+            DropForeignKey("dbo.BasketItems", "BasketId", "dbo.Baskets");
+            DropForeignKey("dbo.BasketItems", "ProductId", "dbo.Products");
+            DropIndex("dbo.OrderItems", new[] { "Order_OrderId" });
+            DropIndex("dbo.BasketItems", new[] { "ProductId" });
+            DropIndex("dbo.BasketItems", new[] { "BasketId" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderItems");
+            DropTable("dbo.Customers");
+            DropTable("dbo.Baskets");
+            DropTable("dbo.Products");
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
